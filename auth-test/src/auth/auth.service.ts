b078b1078f939c554f64e4a5575c65b17cb4a7cf
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-04-13 12:12:07 +0200

    feat(auth.service.ts): ajout hashing mot de passe avec bcrypt

--- auth-test/src/auth/auth.service.ts
+++ auth-test/src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -10,5 +11,19 @@ export class AuthService {
         private readonly userRepo: Repository<User>,
     ) {}
 
+    async signUp(email: string, password: string)
+    {
+        // ** Verification Email
+        const existingUser = await this.userRepo.findOneBy({email});
+        if(existingUser) {
+            throw new Error('Email déjà utilisé')
+        }
+
+        // ** Hashage Mot de passe
+        const hashedPassword = bcrypt.hash(password,10);
+
+
+
+    }
 }
 
