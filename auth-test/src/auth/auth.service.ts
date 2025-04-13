5b3a8e4e8f13ea91f045f6dd6f2091f4da8cb717
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-04-13 11:54:26 +0200

    feat(auth.service.ts): ajout repo User dans auth service

--- auth-test/src/auth/auth.service.ts
+++ auth-test/src/auth/auth.service.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectRepository(User) // ** On injecte le repo, pas l'entité
+        private readonly userRepo: Repository<User>,
+    ) {}
+
+}
 
