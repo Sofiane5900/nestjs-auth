ea527bf7b30d8fa62416083ccd894df4fc3404da
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-04-13 11:34:26 +0200

    feat(app.module.ts): ajout connexion sqlite

--- auth-test/src/app.module.ts
+++ auth-test/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './auth/entity/user.entity';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User], // ? Peut étre ajouter le directory "entity" directement ? 
+      synchronize: true, // ! Ne pas utiliser en PROD, auto-sync les changements
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
