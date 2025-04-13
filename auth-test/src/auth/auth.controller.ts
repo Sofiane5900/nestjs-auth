44600eac7cc1ab3f83401e3e31b028e1d200c0bb
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-04-13 11:24:31 +0200

    feat(auth): route signup fonctionnelle

--- auth-test/src/auth/auth.controller.ts
+++ auth-test/src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignupDto } from './dtos/signup.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   //TODO: POST Signup 
-  // @Post('signup') // auth/signup
- // async signUp(@Body() signupData: SignupDto)
+  @Post('signup') // auth/signup
+  async signUp(@Body() signupData: SignupDto) {}
 
 
 }
