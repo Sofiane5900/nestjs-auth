93918c4bdd76d5226d9ab0133c5771ad921e6e89
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-04-13 11:46:24 +0200

    chore(auth.module.ts): ajout import repo User

--- auth-test/src/auth/auth.module.ts
+++ auth-test/src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])], // injection du repo user 
   controllers: [AuthController],
   providers: [AuthService],
 })
